e5e1f817ddef1e7de7c5db523703794cc57d31ce
FRAZ5094 <frasergmorrison@btinternet.com>
2022-06-03 10:08:32 +0100

    navbar underway

--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -1,15 +1,21 @@
-<nav class="flex flex-row justify-between h-32 bg-red-500">
-  <div class="h-full">
+<script>
+  let isOpen=true;
+
+</script>
+
+<nav class="flex flex-row justify-between h-24 bg-red-500">
+  <div class="">
     <a href="#">
       <img class="h-full" src="/images/RGS_logo.png" />
     </a>
 
   </div>
-  <div class="flex">
-    <a class="border-2" href="#">Home</a>
-    <a class="border-2" href="#">About</a>
-    <a class="border-2" href="#">Contact</a>
-  </div> 
-
+  <ul class="md:inline-flex hidden flex items-center flex-col md:flex-row">
+    <li><a class="hover:text-red-300 border-2 text-2xl mx-4 p-4" href="#">Home</a></li>
+    <li><a class="hover:text-red-300 border-2 text-2xl mx-4 p-4" href="#">Shop</a></li>
+    <li><a class="hover:text-red-300 border-2 text-2xl mx-4 p-4" href="#">About</a></li>
+    <li><a class="hover:text-red-300 border-2 text-2xl mx-4 p-4" href="#">Contact</a></li>
+  </ul> 
+  <img class="inline-flex md:hidden" src="/images/RGS_logo.png" />
 </nav>
 
