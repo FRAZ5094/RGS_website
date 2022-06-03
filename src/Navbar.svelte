78868ba506f7c5832fb0b934a84f04b8ea32e985
FRAZ5094 <frasergmorrison@btinternet.com>
2022-06-03 23:42:49 +0100

    starting redesign of navbar

--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
   let isOpen=true;
-
 </script>
 
-<nav class="flex flex-row justify-between bg-primary">
-  <div class="p-2 w-1/4">
-      <a class="" href="#">
-        <img class="w-20 md:w-full" src="/images/RGS_logo.png" />
-      </a>
-    </div>
+<nav class="flex flex-row justify-evenly bg-primary items-center">
+  <a href="https://www.instagram.com/rearglutespread_/?hl=en" class="p-4">
+    <img src="/images/instagram.svg" class="h-16 md:hidden inline-flex"/>
+  </a>
+  <a class="" href="#">
+    <img class="h-36 md:w-full" src="/images/RGS_logo.png" />
+  </a>
   <ul class="md:inline-flex hidden flex items-center flex-col md:flex-row px-2">
     <li><a class="hover:text-red-300 border-2 text-2xl mx-4 p-4" href="#">Home</a></li>
     <li><a class="hover:text-red-300 border-2 text-2xl mx-4 p-4" href="#">Shop</a></li>
     <li><a class="hover:text-red-300 border-2 text-2xl mx-4 p-4" href="#">About</a></li>
     <li><a class="hover:text-red-300 border-2 text-2xl mx-4 p-4" href="#">Contact</a></li>
   </ul> 
-  <button>
-    <img src="/images/menu.svg" class="p-2 w-20 md:hidden inline-flex"/>
+  <button class="p-4">
+    <img src="/images/menu.svg" class="h-16 md:hidden inline-flex"/>
   </button>
 </nav>
