28da7485c33ab56ab3796aa36fd0de6da58b0bec
FRAZ5094 <frasergmorrison@btinternet.com>
2022-06-06 21:55:29 +0100

    nav bar pops out

---
+++ src/NavMenuItems.svelte
@@ -0,0 +1,6 @@
+<ul class="md:inline-flex bg-primary py-8 flex md:items-center flex-col md:flex-row" >
+    <li><a class="hover:text-red-300 border-2 text-2xl mx-4 py-8" href="#">Home</a></li>
+    <li><a class="hover:text-red-300 border-2 text-2xl mx-4 p-4" href="#">Shop</a></li>
+    <li><a class="hover:text-red-300 border-2 text-2xl mx-4 p-4" href="#">About</a></li>
+    <li><a class="hover:text-red-300 border-2 text-2xl mx-4 p-4" href="#">Contact</a></li>
+  </ul> 
--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -1,6 +1,24 @@
 <script lang="ts">
-  let isOpen=true;
+  import NavMenuItems from "./NavMenuItems.svelte";
+  let open=false;
+  $: outerWidth = 0;
+  $: innerWidth = 0;
+  $: outerHeight = 0;
+  $: innerHeight = 0;
+  $:mobile = outerWidth <=768;
+
+  $:{
+    /*if open and changes to not mobile close*/
+    if (!mobile && open){
+      open = false;
+    }
+  }
+
+  import { fade, blur, fly, slide, scale } from "svelte/transition";
+  import { quintOut } from "svelte/easing";
 </script>
+
+<svelte:window bind:outerWidth bind:innerHeight bind:outerHeight />
 
 <nav class="flex flex-row justify-evenly bg-primary items-center">
   <a href="https://www.instagram.com/rearglutespread_/?hl=en" class="p-4">
@@ -9,13 +27,16 @@
   <a class="" href="#">
     <img class="h-36 md:w-full" src="/images/RGS_logo.png" />
   </a>
-  <ul class="md:inline-flex hidden flex items-center flex-col md:flex-row px-2">
-    <li><a class="hover:text-red-300 border-2 text-2xl mx-4 p-4" href="#">Home</a></li>
-    <li><a class="hover:text-red-300 border-2 text-2xl mx-4 p-4" href="#">Shop</a></li>
-    <li><a class="hover:text-red-300 border-2 text-2xl mx-4 p-4" href="#">About</a></li>
-    <li><a class="hover:text-red-300 border-2 text-2xl mx-4 p-4" href="#">Contact</a></li>
-  </ul> 
-  <button class="p-4">
-    <img src="/images/menu.svg" class="h-16 md:hidden inline-flex"/>
+  {#if !mobile}
+  <NavMenuItems />
+  {/if}
+  <button class="p-4" on:click={() => open = !open}>
+        <img src={open ? "/images/x.svg" : "/images/menu.svg"} class="h-16 md:hidden inline-flex" />
   </button>
 </nav>
+{#if open}
+<NavMenuItems />
+{/if}
+
+<p>{outerWidth}</p>
+<p>{mobile}</p>
